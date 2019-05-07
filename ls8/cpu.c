7dd43713fc442ac3035706ea3490920f0bc9f527
darenlarson <larson.daren@gmail.com>
2019-05-07 15:21:53 -0500

    Done through step 9

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -5,60 +5,61 @@
 
 #define DATA_LEN 6
 
-/**
- * Load the binary bytes from a .ls8 source file into a RAM array
- */
-void cpu_load(struct cpu *cpu)
-{
-  char data[DATA_LEN] = {
-    // From print8.ls8
-    0b10000010, // LDI R0,8
-    0b00000000,
-    0b00001000,
-    0b01000111, // PRN R0
-    0b00000000,
-    0b00000001  // HLT
-  };
+ // Load the binary bytes from a .ls8 source file into a RAM array
+void cpu_load(struct cpu *cpu, char *filename) {
+  FILE *fp = fopen(filename, "r"); // open up the file that was entered in the command line
 
-  int address = 0;
-
-  for (int i = 0; i < DATA_LEN; i++) {
-    cpu->ram[address++] = data[i];
+  if (fp == NULL) { // fp is NULL if the filename entered in the command line doesn't exist
+    fprintf(stderr, "Error opening file: %s\n", filename);
+    exit(2);
   }
 
-  // TODO: Replace this with something less hard-coded
+  int address = 0;
+  
+  char line[1024];
+
+  while (fgets(line, 1024, fp) != NULL) {
+    char *endptr; // keeps track of chars that are not numbers in the file
+
+    unsigned int val = strtoul(line, &endptr, 2); // converts string to number
+
+    if (endptr == line) { // prevents lines with no numbers from being stored on RAM
+      continue;
+    }
+
+    cpu->ram[address++] = val; // stores the values in RAM
+  }
+
+  fclose(fp); // closes the file
 }
 
+
 // ## Step 2: Add RAM functions
+// mar = Memory Address Register. It holds a memory address
+// Goes to the CPU's RAM, at the index specified by mar
 unsigned char cpu_ram_read(struct cpu *cpu, unsigned char mar)
 {
-  // mar = Memory Address Register. It holds a memory address
-
-  // Goes to the CPU's RAM, at the index specified by mar
   return cpu->ram[mar];
 }
 
-// ## Step 2: Add RAM functions
+
+// mdr = Memory Data Register. It holds a value?
+// nothing needs to be returned bc we are just writing to memory
+// writing the mdr to RAM
 void cpu_ram_write(struct cpu *cpu, unsigned char index, unsigned char mdr)
 {
-  // mdr = Memory Data Register. It holds a value?
-  // nothing needs to be returned bc we are just writing to memory
-
-  // writing the mdr to RAM
   cpu->ram[index] = mdr;
 }
 
 
 
 
-/**
- * ALU
- */
+// ALU
 void alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB)
 {
   switch (op) {
     case ALU_MUL:
-      // TODO
+      cpu->reg[regA] *= cpu->reg[regB];
       break;
 
     // TODO: implement more ALU ops
@@ -72,62 +73,60 @@ void cpu_run(struct cpu *cpu)
 {
   int running = 1; // True until we get a HLT instruction
 
-  // initialize operand variables
-  unsigned char operandA, operandB;
+  unsigned char operandA, operandB; // initialize operand variables
 
   while (running) {
-    // TODO
-    // 1. Get the value of the current instruction (in address PC).
-    unsigned char IR = cpu->ram[cpu->pc];
+    unsigned char IR = cpu->ram[cpu->pc]; // 1. Get the value of the current instruction (in address pc).
 
-    // 2. Figure out how many operands this next instruction requires
-    unsigned int num_operands = IR >> 6;
+    unsigned int num_operands = IR >> 6; // 2. Figure out how many operands this next instruction requires. Shifting over 6 bits bc this info is stored in the IR at the far left 2 bits.
 
-    // 3. Get the appropriate value(s) of the operands following this instruction
-    if (num_operands == 2)
-    {
+    if (num_operands == 2) { // 3. Get the appropriate value(s) of the operands following this instruction
       operandA = cpu_ram_read(cpu, cpu->pc + 1);
       operandB = cpu_ram_read(cpu, cpu->pc + 2);
-    }
-    else if (num_operands == 1)
-    {
+
+    } else if (num_operands == 1) {
       operandA = cpu_ram_read(cpu, cpu->pc + 1);
     }
 
-    // 4. switch() over it to decide on a course of action.
-    switch (IR)
+    switch (IR) // 4. switch() over it to decide on a course of action.
     {
-      case LDI:
+      case LDI: // ## Step 6: Add the `LDI` instruction. Set the value of a register to an integer.
+        cpu->reg[operandA] = operandB;
         break;
 
-      case PRN:
+      case PRN: // ## Step 7: Add the `PRN` instruction. Print numeric value stored in the given register.
+        printf("%d\n", cpu->reg[operandA]);
         break;
 
-      // ## Step 5: Implement the `HLT` instruction handler
-      case HLT:
-        // set running to 0 so that we break out of the while loop
+      case HLT: // ## Step 5: Implement the `HLT` instruction handler. Stops the while loop.
         running = 0;
         break;
 
+      case MUL: // ## Step 9: Implement Multiply.
+        alu(cpu, ALU_MUL, operandA, operandB);
+        break;
+
       default:
-        printf("Just Send It");
+        printf("Just Send It!!!");
         exit(1);
         break;
     }
     // 5. Do whatever the instruction should do according to the spec.
-    // 6. Move the PC to the next instruction.
+
+
+
+    cpu->pc += num_operands + 1; // 6. Move the PC to the next instruction.
   }
 }
 
 
 
+
 // ## Step 3: Implement the core of `cpu_init()`
-/**
- * Initialize a CPU struct
- */
+// Initialize a CPU struct
+// TODO: Initialize the PC and other special registers
 void cpu_init(struct cpu *cpu)
 {
-  // TODO: Initialize the PC and other special registers
   cpu = malloc(sizeof(struct cpu));
   cpu->pc = 0;
   memset(cpu->ram, 0, sizeof(cpu->ram));
